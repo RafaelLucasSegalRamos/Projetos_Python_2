f9b38600b94ec6019861d3abfd40fc6a72095582
Rafael <rafaelsegalramos@gmail.com>
2024-01-10 16:51:25 -0300

    update

--- neural_network/KNN.py
+++ neural_network/KNN.py
@@ -1,6 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
 
 dados = pd.DataFrame({
     'A': [1, 1.5, 2, 1.5, -1, -0.5, 0, -0.5],
@@ -32,8 +33,13 @@ vizinhos = vizinhos.fit(x, y)
 X_teste = dados_pred[['A', 'B']]
 
 y_pred = vizinhos.predict(X_teste)
-print(y_pred)
-ax.scatter(dados.A, dados.B, c=dados.y, cmap='viridis')
-ax.scatter(dados_pred.A, dados_pred.B, c='red', marker='s')
-plt.show()
+# ax.scatter(dados.A, dados.B, c=dados.y, cmap='viridis')
+# ax.scatter(dados_pred.A, dados_pred.B, c=y_pred, marker='s')
+# plt.show()
 
+iris = load_iris()
+iris_bd = pd.DataFrame(iris.data, columns=iris.feature_names)
+iris_bd['target'] = iris.target
+iris1 = iris_bd.loc[iris_bd.target.isin([1, 2]), ['petal length (cm)', 'petal width (cm)', 'target']]
+X = iris1.drop('target', axis=1)
+y = iris1['target']
