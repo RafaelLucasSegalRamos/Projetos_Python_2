71cfa0ba01d78a9839d942827a0ecde22e8c4741
Rafael <rafaelsegalramos@gmail.com>
2024-01-25 12:11:10 -0300

    update

--- FastAPI_Duno/main.py
+++ FastAPI_Duno/main.py
@@ -1,18 +1,47 @@
+from authlib.integrations.starlette_client import OAuth
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
+from urllib.parse import urlencode, quote_plus
+
+from starlette.middleware.sessions import SessionMiddleware
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from Secrets import Key  # Neste arquivo possui informações confidenciais de conta, por isso não está no repositório
 
 # Para as variaveis abaixo basta usar o seus dados do Auth0
-domain = Key().domain
-client_id = Key().client_id
-client_secret = Key().client_secret
+domain = Key().DOMAIN
+client_id = Key().CLIENT_ID
+client_secret = Key().CLIENT_SECRET
+secret_key = Key().SECRECT_CODE
+audience = Key().AUDIENCE  # Mesmo sendo puxado de outro arquivo está é uma variavel vazia.
+
+oauth = OAuth()
+oauth.register(
+    name="auth0",
+    client_id=client_id,
+    client_secret=client_secret,
+    server_metadata_url=f"https://{domain}/.well-known/openid-configuration",
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+)
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict()
+    domain: str = domain
+    client_id: str = client_id
+    client_secret: str = client_secret
+
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=secret_key)
 app.mount("/static", StaticFiles(directory="static"), name="static")
+print()
 
 templates = Jinja2Templates(directory="templates")
 
@@ -23,7 +52,50 @@ def root(request: Request):
 
 
 @app.get("/login")
-def login(request: Request):
-    return templates.TemplateResponse(request=request, name="login.html")
+async def login(request: Request):
+    if not 'id_token' in request.session:
+        return await oauth.auth0.authorize_redirect(
+            request,
+            redirect_uri=request.url_for("callback"),
+            audience=audience)
+    return RedirectResponse('/')
+
+
+@app.get("/callback")
+async def callback(request: Request):
+    token = await oauth.auth0.authorize_access_token(request)
+    print(token)
+    request.session['access_token'] = token['access_token']
+    request.session['id_token'] = token['id_token']
+    request.session['userinfo'] = token['userinfo']
+    return RedirectResponse('/profile')
+
+
+@app.get("/logout")
+async def logout(request: Request):
+    response = RedirectResponse(
+        url=f"https://"
+            f"{domain}"
+            f"/v2/logout?"
+            f"{urlencode(
+                {
+                    'returnTo': request.url_for('/'),
+                    'client_id': client_id},
+                quote_via=quote_plus
+            )}"
+    )
+    request.session.clear()
+    return response
+
+
+@app.get("/profile")
+def profile(request: Request):
+    return templates.TemplateResponse(request=request, name="profile.html", context={
+        "request": request,
+        'userinfo': request.session['userinfo']
+
+        }
+    )
+
 
 # Link do video: https://www.youtube.com/watch?v=DeVCIU1JERc&list=TLPQMjMwMTIwMjQl7T2VvcgrMg
---
+++ entendendo_Async/main.py
@@ -0,0 +1,22 @@
+import asyncio
+from time import sleep,strftime
+
+
+async def outraFuncao(p):
+    print(f'teste{p}')
+    await asyncio.sleep(1)
+    print(f"Momento da função {p}: {strftime('%X')}")
+    print(f'teste{p}')
+
+
+async def funcao(p):
+    print(f"Iniciou {strftime('%X')}")
+    task = asyncio.create_task(outraFuncao(1))
+    task2 = asyncio.create_task(outraFuncao(2))
+    await task2
+    await task
+    print('teste')
+    print(f"Terminou {strftime('%X')}")
+
+
+asyncio.run(funcao(1))
